b86c0e838851814d0dcd6ff5e8c641f138cdb47e
ForeverLiveArt <foreverliveart@gmail.com>
2021-01-30 23:07:48 +0200

    the request

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+var { DateTime } = require('luxon');
+DateTime.local();
+
+app.post('/api', (request, response) => {
+	console.log(request.body);
+});
