54fe3464888ef04a07afc633aa6936b47f96f4aa
ForeverLiveArt <foreverliveart@gmail.com>
2021-01-31 15:54:00 +0200

    latest

--- public/script.js
+++ public/script.js
@@ -2,8 +2,6 @@ var DateTime = luxon.DateTime;
 var Dateimp = luxon.DateTime.fromISO;
 var Duration = luxon.Duration;
 var Durimp = luxon.Duration.fromISO;
-
-
 var startDate = 0;
 
 //_____________________________________________________________________make 2d array func
@@ -20,11 +18,11 @@ function make2Darray(cols, rows) {
 //_____________________________________________________________________generate regrets dates array
 
 const regretsAge = ["0,0", "0,9", "0,10", "2,3", "2,4", "4,6", "4,7", "7,6",
-                  "7,6", "11,2", "11,3", "15,8", "15,9", "20,11", "20,12", 
-                  "26,11", "26,12", "33,8", "33,9", "41,2", "41,3", "49,5", 
-                  "49,6", "58,5", "58,6", "68,2", "68,3", "78,8", "78,9", 
-                  "89,11", "89,12", "101,0"
-                 ];
+                    "7,6", "11,2", "11,3", "15,8", "15,9", "20,11", "20,12",
+                    "26,11", "26,12", "33,8", "33,9", "41,2", "41,3", "49,5",
+                    "49,6", "58,5", "58,6", "68,2", "68,3", "78,8", "78,9",
+                    "89,11", "89,12", "101,0"
+];
 //_____________________________________________________________________
 
 //_____________________________________________________________________GENERATE regrets FIXED TABLE
@@ -46,7 +44,7 @@ function makeRegretsArray() {
 
 //______________________________________________________________________SUMM DIGITS OF A NUM
 
-function sumDigits(n) { 
+function sumDigits(n) {
     let sum = 0;
     while (n) {
         digit = n % 10;
@@ -59,7 +57,7 @@ function sumDigits(n) {
 
 //______________________________________________________________________RULE 22
 
-function ttrule(z) { 
+function ttrule(z) {
     z = parseInt(z);
     while (z > 22) {
         z -= 22;
@@ -94,19 +92,16 @@ function checkDate() { //валидация ввода
 
 //______________________________________________________________________
 
-
+//______________________________________________________________________MAIN FUNC
 
 async function calculateOPV(dateString) {
-
     startDate = DateTime.fromFormat(dateString, 'yyyy-MM-dd');
     //var now = luxon.DateTime.local(); 
-
     if (startDate.isValid) { //проверка даты на валидность
         if ($('select#tableSelector option:checked').val() == 1) { //определение выбранной таблицы
             $('#contentregrets').css('display', 'none');
             $('#contentOPV').css('display', 'initial');
         }
-
         if ($('select#tableSelector option:checked').val() == 2) { //определение выбранной таблицы
             $('#contentOPV').css('display', 'none');
             $('#contentregrets').css('display', 'initial');
@@ -124,18 +119,17 @@ async function calculateOPV(dateString) {
 
         const response = await fetch('/api', options);
         const answer = await response.json();
-        console.log(answer.dateCounter);
 
         //______________________________________________________________________
 
         //______________________________________________________________________FIRST TABLE
-        
+
         let i = 0;
         let intervals = 22; //количество сотен в цикле
         let cycle = 1;
 
-            //первая таблица
         $("#table1 tbody tr").remove(); // очистить таблицу перед новой датой
+
         $('#table1').find('tbody').append( //пишет первую дату
             '<tr>' +
             '<td colspan="2 "><h4>' + Dateimp(answer.dateCounter[0]).toFormat('dd.MM.yyyy') + '</h4></td>' +
@@ -147,21 +141,21 @@ async function calculateOPV(dateString) {
             if (i == intervals) { //отделяет 22 цикла
                 intervals += 22;
                 cycle += 1;
-                    $('#table1').find('tbody').append(
-                        '<tr>' +
-                        '<td colspan="2"><h4>' + DateTime.fromISO(answer.dateCounter[i]).year + '</h4></td>' +
-                        '<td colspan="4"><h4>Цикл ' + cycle + '</h4></td>' +
-                        '</tr>'
-                    );
+                $('#table1').find('tbody').append(
+                    '<tr>' +
+                    '<td colspan="2"><h4>' + DateTime.fromISO(answer.dateCounter[i]).year + '</h4></td>' +
+                    '<td colspan="4"><h4>Цикл ' + cycle + '</h4></td>' +
+                    '</tr>'
+                );
             }
-                //cоздает таблицу ОПВ
-            $('#table1').find('tbody').append(
+            
+            $('#table1').find('tbody').append( //cоздает таблицу ОПВ
                 '<tr>' +
-                '<td>' + Dateimp(answer.dateCounter[i]).toFormat('dd.MM.yyyy') + ' – ' + 
-                         Dateimp(answer.dateCounter[i + 1]).toFormat('dd.MM.yyyy') + '</td>' +
-                '<td>' + Durimp(answer.diffInTime[i]).years.toFixed(0) + ', ' + 
-                         Durimp(answer.diffInTime[i]).months.toFixed(0) + ', ' + 
-                         Durimp(answer.diffInTime[i]).days.toFixed(0) + '</td>' +
+                '<td>' + Dateimp(answer.dateCounter[i]).toFormat('dd.MM.yyyy') + ' – ' +
+                Dateimp(answer.dateCounter[i + 1]).toFormat('dd.MM.yyyy') + '</td>' +
+                '<td>' + Durimp(answer.diffInTime[i]).years.toFixed(0) + ', ' +
+                Durimp(answer.diffInTime[i]).months.toFixed(0) + ', ' +
+                Durimp(answer.diffInTime[i]).days.toFixed(0) + '</td>' +
                 '<td>' + answer.opv1[i] + '</td>' + '<td>' + answer.opv2[i] + '</td>' + '<td>' + answer.opv3[i] + '</td>' +
                 '</tr>'
             );
@@ -172,22 +166,18 @@ async function calculateOPV(dateString) {
         //______________________________________________________________________SECOND TABLE
 
         $("#table2 tbody tr").remove();
-        let j = 0;
-        let k = 0;
-        while (j < regretsAge.length) {
+
+        for (let k = 0, j = 0; j < regretsAge.length; k++, j = j + 2) {
             $('#table2').find('tbody').append(
-                '<tr>' + 
+                '<tr>' +
                 '<td>' + startDate.plus({ years: regretsAge[j].split(",")[0], month: regretsAge[j].split(",")[1] }).toFormat('dd.MM.yyyy') +
-                 ' – ' + startDate.plus({ years: regretsAge[j + 1].split(",")[0], month: regretsAge[j + 1].split(",")[1] }).toFormat('dd.MM.yyyy') + 
+                 ' – ' + startDate.plus({ years: regretsAge[j + 1].split(",")[0], month: regretsAge[j + 1].split(",")[1] }).toFormat('dd.MM.yyyy') +
                 '<td>' + regretsAge[j] + ' – ' + regretsAge[j + 1] + '</td>' +
                 '<td>' + answer.regrets[k] + '</td>' +
                 '</tr>'
             );
-            j = j + 2;
-            k = k + 1;
         }
         //______________________________________________________________________
-
 
     } else {
         $('#alerts').prepend( //текст ошибки даты
