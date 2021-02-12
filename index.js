8fd0c704b1c55f914f0218f82db3244437e948cb
ForeverLiveArt <foreverliveart@gmail.com>
2021-02-12 19:07:58 +0200

    update

--- index.js
+++ index.js
@@ -5,17 +5,11 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
 
-
-
-
-
-
 var { DateTime } = require('luxon');
 DateTime.local();
 
 const { ttrule, make2Darray, regretsArray, regretsAge, sumDigits } = require('./app_tools.js');
 var startDate = 0;
-
 
 app.post('/api', (request, response) => {
 
@@ -101,8 +95,6 @@ app.post('/kids_api', (request, response) => {
     let yearArcane = ttrule(sumDigits(startDate.year));
     //______________________________________________________________________OPV 0 CALC
     let opv1 = [];
-    let opv2 = [];
-    let opv3 = [];
     opv1[0] = ttrule(dayArcane - monthArcane);
     //______________________________________________________________________CALC VALUES
     let ieb = ttrule(yearArcane - monthArcane);
@@ -133,33 +125,24 @@ app.post('/kids_api', (request, response) => {
 
     let diffInTime = [];
     let i = 0;
-    while (i != 220) { //Цикл заполнения массива опв
+    while (i != 22) { //Цикл заполнения массива опв
         dateCounter[i + 1] = dateCounter[i].plus({ days: 100 });
         opv1[i + 1] = ttrule(opv1[i] - 1);
-        opv2[i] = ttrule(opv1[i] - kch);
-        opv3[i] = ttrule(opv1[i] + opv2[i] + kch + ieb);
         //считает возраст в годах и месяцах
-        diffInTime[i] = dateCounter[i + 1].diff(dateCounter[0], ['years', 'months', 'days']);
+        diffInTime[i] = dateCounter[i + 1].diff(dateCounter[0], ['years', 'months', 'days', 'minutes', 'seconds']);
         diffInTime[i].toObject();
         i = i + 1;
     }
-
     //______________________________________________________________________REGRETS        
 
-    let regrets = [];
-    regrets[0] = ttrule(regretsArray[ttrule(parseInt(startDate.month)) - 1][ttrule(parseInt(startDate.day)) - 1] + opv1[0]);
-    for (let k = 0, j = 0; j < regretsAge.length; k++, j = j + 2) {
-        regrets[k + 1] = ttrule(regrets[k] + 1);
-    }
+    let regrets = ttrule(regretsArray[ttrule(parseInt(startDate.month)) - 1][ttrule(parseInt(startDate.day)) - 1] + opv1[0]);
+
     //______________________________________________________________________
     //______________________________________________________________________SEND RESPONSE
     response.json({
         dateCounter: dateCounter,
         opv1: opv1,
-        opv2: opv2,
-        opv3: opv3,
         regrets: regrets,
         diffInTime: diffInTime,
-        regretsAge: regretsAge
     });
 });
--- public/kids_numerology/kids_script.js
+++ public/kids_numerology/kids_script.js
@@ -7,8 +7,14 @@ var startDate = 0;
 
 //______________________________________________________________________AUTO TAB FIELDS
 
+function leadingZeros(input) {
+    if(!isNaN(input.value) && input.value.length === 1) {
+      input.value = '0' + input.value;
+    }
+  }
+
 function moveOnMax(field, nextFieldID) {
-    if (field.value.length >= field.maxLength) {
+    if (field.value.length === field.maxLength) {
         document.getElementById(nextFieldID).focus();
     }
 }
@@ -54,38 +60,69 @@ async function calculateOPV(dateString) {
         $('#spinners').css('display', 'initial');
 
         $("#table1 tbody tr").remove(); // очистить таблицу перед новой датой
+        $("#learnage_container").empty();
 
-        $('#table1').find('tbody').append( //пишет первую дату
-            '<tr>' +
-            '<td colspan="2 "><h4>' + Dateimp(answer.dateCounter[0]).toFormat('dd.MM.yyyy') + '</h4></td>' +
-            '<td colspan="4"><h4>Цикл ' + cycle + '</h3></td>' +
-            '</tr>'
+        $("#learnage_container").append(
+            '<h4>Возраст начала обучения: '+ answer.opv1[0]*0.27 + '</h4>'
         );
 
-        //cюда добавить первое описание
+        $('#table1').find('tbody').append( //пишет первую дату и столбец
 
-        while (i != 220) { //Цикл заполнения таблицы
-            if (i == intervals) { //отделяет 22 цикла
-                intervals += 22;
-                cycle += 1;
-                $('#table1').find('tbody').append(
-                    '<tr>' +
-                    '<td colspan="2"><h4>' + DateTime.fromISO(answer.dateCounter[i]).year + '</h4></td>' +
-                    '<td colspan="4"><h4>Цикл ' + cycle + '</h4></td>' +
-                    '</tr>'
-                );
-            }
-            
+            '<tr data-bs-toggle="collapse" id="tr_slide0"' +
+            'data-bs-target="#accord0"' +
+            'aria-controls="accord0">' +
+                    '<td>' + Dateimp(answer.dateCounter[i]).toFormat('dd.MM.yyyy') + ' – ' +
+                            Dateimp(answer.dateCounter[i + 1]).toFormat('dd.MM.yyyy') + '</td>' +
+                    '<td>' + Durimp(answer.diffInTime[i]).years.toFixed(0) + ', ' +
+                            Durimp(answer.diffInTime[i]).months.toFixed(0) + ', ' +
+                            Durimp(answer.diffInTime[i]).days.toFixed(0) + '</td>' +
+                    '<td>' + answer.opv1[i] + '</td>' +
+            '</tr>' +
+            '<tr>' +
+                '<td colspan="3" class="collapse show multi-collapse mt-3" data-bs-parent="#table1" id="accord0">' +
+                    '<div class="fs-6 p-3" style="background-color:white!important;">' +
+                        '<h3>Знак ' + answer.opv1[i] + '</h3>' +
+                        '<article class="readmore">' +
+                        kidsTextArray[0][answer.opv1[i]] +
+                    '</article><a href="#tr_slide' + i + '" role="button" data-bs-toggle="collapse"' +
+                    'data-bs-target="#accord1"' +
+                    'aria-controls="#accord1" type="button" class="btn btn-outline-primary scroller">Следующий знак →</a></div>' +
+                '</td>' +
+            '</tr>'            
+        );
+        i = i + 1;
+
+        while (i != 22) { //Цикл заполнения таблицы
+           
             $('#table1').find('tbody').append( //cоздает таблицу ОПВ
-                '<tr>' +
+            '<tr data-bs-toggle="collapse" id="tr_slide' + i + '"' +
+                'data-bs-target="#accord' + i + '"' +
+                'aria-expanded="false"' +
+                'aria-controls="accord' + i + '">' +
                 '<td>' + Dateimp(answer.dateCounter[i]).toFormat('dd.MM.yyyy') + ' – ' +
-                Dateimp(answer.dateCounter[i + 1]).toFormat('dd.MM.yyyy') + '</td>' +
+                         Dateimp(answer.dateCounter[i + 1]).toFormat('dd.MM.yyyy') + '</td>' +
                 '<td>' + Durimp(answer.diffInTime[i]).years.toFixed(0) + ', ' +
-                Durimp(answer.diffInTime[i]).months.toFixed(0) + ', ' +
-                Durimp(answer.diffInTime[i]).days.toFixed(0) + '</td>' +
-                '<td>' + answer.opv1[i] + '</td>' + '<td>' + answer.opv2[i] + '</td>' + '<td>' + answer.opv3[i] + '</td>' +
-                '</tr>'
-
+                         Durimp(answer.diffInTime[i]).months.toFixed(0) + ', ' +
+                         Durimp(answer.diffInTime[i]).days.toFixed(0) + '</td>' +
+                '<td>' + answer.opv1[i] + '</td>' +
+            '</tr>' +
+            '<tr>' +
+                '<td colspan="3" class="collapse multi-collapse mt-3" data-bs-parent="#table1" id="accord' + i + '">' +
+                    '<div class="fs-6 p-3" style="background-color:white!important;">' +
+                    '<h3>Знак ' + answer.opv1[i] + '</h3>' +
+                    '<article class="readmore">' +
+                        kidsTextArray[answer.opv1[i]] +
+                    '</article>' +
+                    (i != 21 ?  //последняя кнопка ведет на первый знак
+                        '<a href="#tr_slide' + i + '" role="button" data-bs-toggle="collapse"' +
+                        'data-bs-target="#accord' + (i + 1) + '"' +
+                        'aria-controls="#accord' + (i + 1) + '" type="button" class="btn btn-outline-primary scroller">Следующий знак →</a></div>'
+                        : 
+                        '<a href="#table1" role="button" data-bs-toggle="collapse"' +
+                        'data-bs-target="#accord0"' +
+                        'aria-controls="#accord0" type="button" class="btn btn-outline-primary scroller">Первый знак →</a></div>') +
+                '</td>' +
+            '</tr>'
             );
             i = i + 1;
         }
@@ -93,28 +130,13 @@ async function calculateOPV(dateString) {
         //______________________________________________________________________
         //______________________________________________________________________SECOND TABLE
 
-        $("#table2 tbody tr").remove();
+        $("#regrets_container").empty();
 
-        for (let k = 0, j = 0; j < answer.regretsAge.length; k++, j = j + 2) {
-            $('#table2').find('tbody').append(
-                '<tr data-bs-toggle="collapse"' +
-                    'data-bs-target="#collapseExample' + k + '"' +
-                    'aria-expanded="false"' +
-                    'aria-controls="collapseExample' + k + '">' +
-                        '<td>' + startDate.plus({ years: answer.regretsAge[j].split(",")[0], month: answer.regretsAge[j].split(",")[1] }).toFormat('dd.MM.yyyy') +
-                        ' – ' + startDate.plus({ years: answer.regretsAge[j + 1].split(",")[0], month: answer.regretsAge[j + 1].split(",")[1] }).toFormat('dd.MM.yyyy') +
-                        '<td>' + answer.regretsAge[j] + ' – ' + answer.regretsAge[j + 1] + '</td>' +
-                        '<td>' + answer.regrets[k] + '</td>' +
-                '</tr>' +
-                '<tr>' +
-                    '<td colspan="3" class="collapse multi-collapse mt-3" id="collapseExample' + k + '">' +
-                        '<div class="fs-6 p-3" ><article class="readmore">' +
-                            regretsTextArray[answer.regrets[k]] +
-                        '</article></div>' +
-                    '</td>' +
-                '</tr>'
-            );
-        }
+        $("#regrets_container").append(
+            '<h4>Знак сожалений ребёнка — ' + answer.regrets +'</h4>' +
+            '<article>' + regretsTextArray[answer.regrets] + '</article>'
+        );
+
         //_____________________________________________________________________
 
         $readMoreJS.init({
@@ -122,19 +144,23 @@ async function calculateOPV(dateString) {
               numOfWords: 45,     
               // If true, user can toggle between 'read more' and 'read less'. 
               toggle: true,              
-              moreLink: 'читать далее ...', 
-              lessLink: 'свернуть',      
+              moreLink: 'Читать далее ↓', 
+              lessLink: 'Cвернуть ↑',      
               // The class given to the read more link. 
-              linkClass: 'rm-link',    
+              linkClass: 'btn btn-outline-primary',    
               // The class given to the div container of the read more link.
-              containerClass: false
+              containerClass: 'readmorecls'
         });    
         //_____________________________________________________________________CONTROL DISPLAY OPTIONS
         $('#spinners').css('display', 'none');
         $('#myTabContent').css('display', 'initial');
         $('#tabulation').css('display', 'initial');
         //_____________________________________________________________________
+        $('.scroller').click(function() { //следующий знак прокрутка страницы
+            let sectionTo = $(this).attr('href');
+            $.scrollTo(sectionTo);
 
+        });
 
     } else {
         $('#spinners').css('display', 'none');
